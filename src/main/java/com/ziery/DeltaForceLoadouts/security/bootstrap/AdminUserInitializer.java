d73eebdbad2bfcd1dd02d0623631bbec9d067302
Ziery-Dev <zierydev@gmail.com>
2026-02-14 15:15:28 -0300

    chore(UserInitializer) Initializer bootstrap  desativado

--- src/main/java/com/ziery/DeltaForceLoadouts/security/bootstrap/AdminUserInitializer.java
+++ src/main/java/com/ziery/DeltaForceLoadouts/security/bootstrap/AdminUserInitializer.java
@@ -5,6 +5,7 @@ import com.ziery.DeltaForceLoadouts.security.entity.User;
 import com.ziery.DeltaForceLoadouts.security.entity.UserRoles;
 import com.ziery.DeltaForceLoadouts.security.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
@@ -16,15 +17,35 @@ public class AdminUserInitializer implements CommandLineRunner {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
-    @Override
+
+    @Value("${app.bootstrap-admin:false}")
+    private boolean enabled;
+
+    @Value("${admin.username:}")
+    private String adminUsername;
+    @Value("${admin.password:}")
+    private String adminPassword;
+    @Value("${admin.email:}")
+    private String adminEmail;
+
     public void run(String... args) {
-        String adminUsername = System.getenv().getOrDefault("ADMIN_USERNAME", "admin");
-        String adminPassword = System.getenv().getOrDefault("ADMIN_PASSWORD", "admin123");
-        String adminEmail    = System.getenv().getOrDefault("ADMIN_EMAIL", "admin@local");
+        if (!enabled) return;
+
+        if (adminUsername == null || adminUsername.isBlank()) {
+            throw new IllegalStateException("Username admin vazio ou nulo!");
+        }
+        if (adminEmail == null || adminEmail.isBlank()) {
+            throw new IllegalStateException("Email admin vazio ou nulo!");
+        }
+        if (adminPassword == null || adminPassword.isBlank()) {
+            throw new IllegalStateException("Senha admin password vazio ou nulo!");
+        }
 
         if (userRepository.findByUsername(adminUsername).isPresent()) {
             return;
         }
+
+
 
         User admin = new User();
         admin.setUsername(adminUsername);
@@ -34,6 +55,6 @@ public class AdminUserInitializer implements CommandLineRunner {
 
         userRepository.save(admin);
 
-        System.out.println("ADMIN criado: " + adminUsername);
+        System.out.println("ADMIN criado com sucesso!");
     }
 }
