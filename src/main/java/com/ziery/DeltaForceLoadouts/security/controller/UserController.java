f7a3b398fa776a1360ff5bd2c683b625cda16273
Ziery-Dev <zierydev@gmail.com>
2026-03-06 00:46:07 -0300

    fix(UserControle): correção de url repetida que causava bug

--- src/main/java/com/ziery/DeltaForceLoadouts/security/controller/UserController.java
+++ src/main/java/com/ziery/DeltaForceLoadouts/security/controller/UserController.java
@@ -45,7 +45,7 @@ public class UserController {
         return ResponseEntity.ok(userDtoResponse);
     }
 
-    @GetMapping("/users")
+    @GetMapping()
     public Page<UserDtoResponse> getAllUsers(
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size,
